1b94b69f5e5b85e1208c393d7ec5a25cc5f4dcb8
sebamontini <sebastianmontini@gmail.com>
2012-06-13 15:28:48 -0300

    added awesome config to git repo

---
+++ autorun.lua
@@ -0,0 +1,4 @@
+run_once("xscreensaver", "-no-splash")         -- starts screensaver daemon 
+run_once("setxkbmap", "us") -- keyboard en inlges
+run_once("xbindkeys")
+run_once("nm-applet") -- networking
